029b48e225115e1997f99bbea09009e1194ca744
Gokhan1979 <gokhan1979@live.be>
2026-09-20 10:45:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// For now just test
+console.log("POP THE LOOK loaded");
+
+// Sample products - later admin will add
+const products = [
+  { name: "Sample Hoodie", price: "1999" }
+];
+
+console.log(products);
